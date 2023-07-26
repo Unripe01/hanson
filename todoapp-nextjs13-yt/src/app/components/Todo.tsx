c6f0dc521e734b704b5dadcd832543109aa7e21d
unripe01 <tomato.ripe.dev@gmail.com>
2023-07-26 17:24:24 +0900

    Todoアプリを作りながらNext.js13の新機能を理解してみよう ~Next.js13入門~

---
+++ todoapp-nextjs13-yt/src/app/components/Todo.tsx
@@ -0,0 +1,70 @@
+"use client"
+import React, { ChangeEvent, use, useEffect, useRef, useState } from "react"
+import { Task } from "../types"
+import { deleteTodo, editTodo } from "../api"
+
+interface TodoProps {
+  todo: Task
+}
+
+const Todo = ({ todo }: TodoProps) => {
+  const ref = useRef<HTMLInputElement>(null)
+  const [isEditing, setIsEditing] = useState(false)
+  const [editedTaskText, setEditedTaskText] = useState(todo.text)
+
+  useEffect(() => {
+    if (isEditing) {
+      ref.current?.focus()
+    }
+  }, [isEditing])
+
+  const handleEdit = async () => {
+    setIsEditing(true)
+  }
+
+  const handleSave = async () => {
+    await editTodo(todo.id, editedTaskText)
+    setIsEditing(false)
+  }
+
+  const handleDelete = async () => {
+    await deleteTodo(todo.id)
+  }
+
+  return (
+    <li
+      key={todo.id}
+      className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow"
+    >
+      {isEditing ? (
+        <input
+          ref={ref}
+          type="text"
+          className="mr-2 py-1 px-2 rounded border border-gray-400"
+          value={editedTaskText}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEditedTaskText(e.target.value)
+          }
+        ></input>
+      ) : (
+        <span>{todo.text}</span>
+      )}
+      <div>
+        {isEditing ? (
+          <button className="text-blue-500 mr-3" onClick={handleSave}>
+            Save
+          </button>
+        ) : (
+          <button className="text-green-500 mr-3" onClick={handleEdit}>
+            Edit
+          </button>
+        )}
+        <button className="text-red-500" onClick={handleDelete}>
+          Delete
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default Todo
